0991479a8b75d482af768d23dd05f84e6f5e4a9d
amrullah <amrullah.zunzunia@gmail.com>
2020-08-08 19:34:52 +0530

    Basic Hello World Program in Java

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Amrullah!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println("My Total: " + Integer.toString(myTotal));
+        System.out.println("My Last One: " + Integer.toString(myLastOne));
+    }
+}
