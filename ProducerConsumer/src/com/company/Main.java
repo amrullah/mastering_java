17660e57d8def5a7596eb7a252a7c8f4b562a600
amrullah <amrullah.zunzunia@gmail.com>
2021-10-24 18:03:32 +0530

    ReEntrantLock in try-finally block

--- ProducerConsumer/src/com/company/Main.java
+++ ProducerConsumer/src/com/company/Main.java
@@ -41,10 +41,11 @@ class MyProducer implements Runnable {
             try {
                 System.out.println(color + "Adding... " + num);
                 bufferLock.lock();
-//                synchronized (buffer) {
-                buffer.add(num);
-//                }
-                bufferLock.unlock();
+                try {
+                    buffer.add(num);
+                } finally {
+                    bufferLock.unlock();
+                }
 
                 Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
@@ -54,10 +55,11 @@ class MyProducer implements Runnable {
 
         System.out.println(color + "Adding EOF and exiting...");
         bufferLock.lock();
-//        synchronized (buffer) {
-        buffer.add(Main.EOF);
-//        }
-        bufferLock.unlock();
+        try {
+            buffer.add(Main.EOF);
+        } finally {
+            bufferLock.unlock();
+        }
     }
 }
 
@@ -76,21 +78,21 @@ class MyConsumer implements Runnable {
     public void run() {
         while (true) {
             bufferLock.lock();
-//            synchronized (buffer) {
-            if (buffer.isEmpty()) {
+            try {
+                if (buffer.isEmpty()) {
+                    continue;
+                }
+                if (buffer.get(0).equals(Main.EOF)) {
+                    System.out.println(color + "Exiting");
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
+            } finally {
                 bufferLock.unlock();
-                continue;
             }
-            if (buffer.get(0).equals(Main.EOF)) {
-                System.out.println(color + "Exiting");
-                break;
-            } else {
-                System.out.println(color + "Removed " + buffer.remove(0));
-                bufferLock.unlock();
-            }
-//            }
 
         }
-        bufferLock.unlock();
+
     }
 }
