09b2659571cbde9dd474d89c21e06660ba3f188f
amrullah <amrullah.zunzunia@gmail.com>
2021-10-25 12:58:45 +0530

    Deadlock solution 1: acquire the locks in the same order

--- DeadLock/src/com/company/Main.java
+++ DeadLock/src/com/company/Main.java
@@ -31,20 +31,20 @@ public class Main {
 
     private static class Thread2 extends Thread {
         public void run() {
-            synchronized (lock2) {
-                System.out.println("Thread 2 has lock 2");
+            synchronized (lock1) {
+                System.out.println("Thread 2 has lock 1");
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
 
                 }
-                System.out.println("Thread 2 waiting for lock 1");
-                synchronized (lock1) {
+                System.out.println("Thread 2 waiting for lock 2");
+                synchronized (lock2) {
                     System.out.println("Thread 2 has lock 1 and 2");
                 }
-                System.out.println("Thread 2 released lock 1");
+                System.out.println("Thread 2 released lock 2");
             }
-            System.out.println("Thread 2 released lock 2");
+            System.out.println("Thread 2 released lock 1");
             System.out.println("Thread 2 Exiting...");
         }
     }
